cb909a92bad946b18a8fb413c9c5251dc6849022
Robin Bolscher <robinbolscher@gmail.com>
2016-01-11 12:14:35 +0100

    Adds stub implementation of qwirkle server and lobby

---
+++ src/Lobby.java
@@ -0,0 +1,8 @@
+
+public Lobby(){
+        public Lobby();
+        public List<Player>;
+        public void addPlayer(Player player);
+        public void findGame();
+        public Game startGame(Players[] players);
+}
---
+++ src/QwirkleServer.java
@@ -0,0 +1,8 @@
+
+public class QwirkleServer(){
+        public QwirkleServer();
+        public boolean startServer();
+        public Lobby createLobby();
+        public void listenForClients();
+        public Player createPlayer();
+}
