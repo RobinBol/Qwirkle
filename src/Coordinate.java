0ff765597254fe0b40306963c726da68b88f6618
elborio <elborio@hotmail.com>
2016-01-12 15:51:50 +0100

    Coordinate static function for map call

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,4 +1,4 @@
-public class Coordinate {
+public final class Coordinate {
 
     public Coordinate(int x, int y){
 
@@ -9,4 +9,14 @@ public class Coordinate {
 
     //Override
     public void equals(){};
+    
+    /*
+     * Returns an integer to pas into the hashmap the contains the keys to tiles that may or may not contain a stone.
+     * Usage hashmap.get(Coordinate.getCoordinateHash(x, y));
+     * Will return the value<Stone>; 
+     */
+    public static int getCoordinateHash(int x, int y) {
+    	return x * 10000 + y;
+    }
+    
 }
